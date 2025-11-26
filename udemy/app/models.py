f42aca4c2f1c61002b610359ee05f7363962c5ce
shivamkumar93 <bcashivam11@gmail.com>
2025-11-26 22:52:14 +0530

    create model

--- udemy/app/models.py
+++ udemy/app/models.py
@@ -1,7 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Name(models.Model):
-    name = models.CharField(max_length=20)
+
+
+class Category(models.Model):
+   name = models.CharField(max_length=100)
+
+   def __str__(self):
+       return self.name
+
+class Course(models.Model):
+    category = models.ManyToManyField(Category,related_name='category', null=True, blank=True ) 
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    language = models.CharField(max_length=50, default='English')
+    published = models.BooleanField(default=False)
+    course_language = models.ManyToManyField("Programminglanguage", related_name='programming' , blank=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+class Topic(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+
+class Programminglanguage(models.Model):
+    name = models.CharField(max_length=50)
+
 
--- udemy/udemy/urls.py
+++ udemy/udemy/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
 
-    # user 
-    path("",home,name="home")
+
 ]
