33451496acb4029104e6a0be066f59db4013f797
somasha06 <somasha09@gmil.com>
2025-11-27 11:12:00 +0530

    creating serializers viewset and path

--- udemy/udemy/urls.py
+++ udemy/udemy/urls.py
@@ -18,10 +18,17 @@ from django.contrib import admin
 from django.urls import path,include
 from app.views import *
 
+router=routers.DefaultRouter()
+router.register(r'Category',Categoryviewset)
+router.register(r'Course',Courseviewset)
+router.register(r'Topic',Topicviewset)
+router.register(r'Programminglanguage',Programminglangviewset)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-
+    path("",include(router.urls)),
 
 
 ]
