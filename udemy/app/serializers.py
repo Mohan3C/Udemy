27ee143467be8245c0103abdd0bc6351dd776160
Mohan <mohan845sharma45@gmail.com>
2025-11-29 08:47:13 +0530

    registration and  login

--- udemy/app/serializers.py
+++ udemy/app/serializers.py
@@ -2,10 +2,15 @@ from rest_framework import serializers,viewsets,routers
 from .models import Category,Course,Topic,Programminglanguage
 from django.contrib.auth.models import User
 
-class Userserializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
 
 class Categoryserializer(serializers.ModelSerializer):
     class Meta:
